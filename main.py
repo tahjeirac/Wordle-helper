60f4b8b6c99654e6a63d713dbd8466e0953e110b
tahjeirac <tahjeirac@users.noreply.github.com>
2022-01-24 23:15:26 -0500

    added some filtering

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import requests
+
+
+def constraint(result):
+    print(result["word"])
+    return True
+
+
 url = "https://api.datamuse.com/words?sp="
 letters = input(
     "what letters do you know, use question marks for unknown spaces: ")
 url += letters
+
+contains = input("what letters are contained in it?")
 res = requests.get(url)
-print(res.json())
+
+words = res.json()
+if contains:
+    filtered_words = print(list(filter(constraint, words)))
+    print(filtered_words)
+else:
+    print(res.json())
+
 
 # credit to datamuse for use of their api
